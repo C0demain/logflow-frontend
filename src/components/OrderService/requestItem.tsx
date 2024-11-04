38e591c0f5a287e8391b84e9e8dce18d6df480be
Cauã Dezidera <dezideracaua@gmail.com>
2024-11-04 09:05:52 -0300

    fix: ajuste e responsividade nos botões de 'histórico' e  'tarefa' na página de ordem e serviço

--- src/components/OrderService/requestItem.tsx
+++ src/components/OrderService/requestItem.tsx
@@ -51,51 +51,55 @@ export const RequestItem: React.FC<RequestItemProps> = ({
   }, [sector]);
 
   return (
-    <div className="flex flex-col sm:flex-row p-4 sm:p-5 rounded-md w-full h-auto bg-white shadow-lg transition-all hover:bg-gray-100">
-     
+    <div className="flex flex-col sm:flex-row p-4 sm:p-5 rounded-md w-full bg-white shadow-lg transition-all hover:bg-gray-100">
+  
       <div className="w-full flex flex-col justify-between mb-4 sm:mb-0 sm:mr-4">
         <div className="flex flex-col space-y-1 mb-4">
           <h1 className="text-lg sm:text-2xl font-bold">{name}</h1>
           <h1 className="text-base sm:text-lg">Cliente: {clientName}</h1>
           <h1 className="text-base sm:text-lg">Preço: {price}</h1>
         </div>
-          {crudAutorizado && <DeleteOrder id={orderId} />}
-       </div>
 
-  
-        <div className="flex justify-center items-center mb-4 sm:mb-0 sm:mr-4">
-          <StageLine stage1={stage1} stage2={stage2} stage3={stage3} />
-        </div>
-
- 
-        <div className="flex flex-col items-center sm:items-end w-full space-y-4 sm:space-y-0 sm:justify-between">
+    
+        <div className="flex justify-center mb-4">
           <div
             className={`badge h-6 ${
               status === "FINALIZADO" ? "badge-success" : "badge-info"
             }`}
-           >
+          >
             <p className="text-sm sm:text-base font-medium">{status}</p>
+          </div>
         </div>
 
-        {userId && orderId && (
-          <div className="flex flex-col sm:flex-row w-full sm:w-auto bg-blue-600 rounded-md h-auto sm:h-1/3">
+        {crudAutorizado && <DeleteOrder id={orderId} />}
+      </div>
+
+  
+      <div className="flex justify-center items-center mb-4 sm:mb-0 sm:mr-4">
+        <StageLine stage1={stage1} stage2={stage2} stage3={stage3} />
+      </div>
+
+ 
+      {userId && orderId && (
+        <div className="flex flex-col sm:flex-row items-center sm:items-end w-full sm:justify-end">
+          <div className="flex flex-col sm:flex-row items-center sm:self-end sm:space-x-1 mt-4 sm:mt-0"> {/* Espaçamento reduzido apenas em telas grandes */}
+            
             <Link
-              className="w-full sm:w-3/5 h-10 flex justify-center items-center rounded-md bg-blue-600 hover:bg-blue-500 transition-all"
+              className="w-full sm:w-28 h-10 flex justify-center items-center rounded-md bg-blue-600 hover:bg-blue-500 transition-all mb-2 sm:mb-0"
               href={`/auth/history`}
             >
               Histórico
             </Link>
-            <Divider orientation="horizontal" size="lg" className="sm:h-full" />
+            
             <Link
-              className="w-full sm:w-3/5 h-10 flex justify-center items-center rounded-md bg-blue-600 hover:bg-blue-500 transition-all"
+              className="w-full sm:w-28 h-10 flex justify-center items-center rounded-md bg-blue-600 hover:bg-blue-500 transition-all"
               href={`/auth/todolist/${userId}/${orderId}`}
             >
               Tarefas
             </Link>
           </div>
-        )}
+        </div>
+      )}
     </div>
-</div>
-
   );
 };
