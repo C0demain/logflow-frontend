3c84c7235ea4dc3eb05336cbf4fca72495c53751
BrunoVieira003 <brunosilverio01@gmail.com>
2024-11-06 20:53:57 -0300

    feat: add selects extras

---
+++ src/components/SelectRole.tsx
@@ -0,0 +1,56 @@
+import { listClient } from "@/app/api/clientService/listClient";
+import { listRoles } from "@/app/api/userService/listRoles";
+import { Sector } from "@/enums/sector";
+import { useEffect, useState } from "react";
+import Select from "react-select";
+
+type propsType = {
+    controlState: any[],
+    className?: string,
+    sector: `${Sector}`
+}
+
+export const SelectRole = (props: propsType) =>{
+    const {sector} = props
+    const [options, setOptions] = useState<any[]>([])
+    const [controlState, setControlState] = props.controlState
+
+    const getRoles = async () => {
+        const response = await listRoles()
+        const options = []
+        let first;
+        for(const c of response){
+            if(c.sector === sector){
+                if(!first){
+                    first = c.id
+                }
+
+                options.push({
+                    value: c.id,
+                    label: c.name
+                })
+            }
+        }
+
+        setControlState(first)
+        setOptions(options)
+        
+    };
+    
+    useEffect(()=>{
+        getRoles()
+        console.log(controlState)
+    }, [sector])
+
+    return (
+        <select
+            value={controlState}
+            onChange={(e) => { setControlState(e.target.value); } }
+            className="select select-bordered w-full"
+            name="stage"
+            key="stage"
+        >
+            {options.map(op => <option value={op.value}>{op.label}</option>)}
+        </select>
+    )
+}
---
+++ src/components/SelectSector.tsx
@@ -0,0 +1,33 @@
+import { listClient } from "@/app/api/clientService/listClient";
+import { useEffect, useState } from "react";
+import Select from "react-select";
+
+type propsType = {
+    controlState: any[],
+    className?: string
+}
+
+const SelectSector = (props: propsType) =>{
+    const options = [
+        { value: "OPERACIONAL", label: "Operacional" },
+        { value: "FINANCEIRO", label: "Financeiro" },
+        { value: "RH", label: "RH" },
+        { value: "DIRETORIA", label: "Diretoria" },
+        { value: "VENDAS", label: "Vendas" },
+    ];
+    const [controlState, setControlState] = props.controlState
+    
+    return (
+        <select
+            value={controlState}
+            onChange={(e) => { setControlState(e.target.value); } }
+            className="select select-bordered w-full"
+            name="sector"
+            key="sector"
+        >
+            {options.map(op => <option value={op.value}>{op.label}</option>)}
+        </select>
+    )
+}
+
+export default SelectSector
