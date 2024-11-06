9ec40a3c72caea62a89f878891df1c38110fb33c
BrunoVieira003 <brunosilverio01@gmail.com>
2024-11-06 18:15:34 -0300

    feat: add criação de processos

--- src/app/auth/process/page.tsx
+++ src/app/auth/process/page.tsx
@@ -1,12 +1,43 @@
 "use client";
 
+import { registerProcess } from "@/app/api/process/registerProcess";
+import CreateButton from "@/components/createButton";
+import ProcessForm from "@/components/ProcessService/ProcessForm";
 import { ProcessesList } from "@/components/ProcessService/ProcessList";
+import { Process } from "@/interfaces/process";
+import { useToast } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
 
 export default function ProcessListPage() {
+  const toast = useToast()
+  const router = useRouter()
+  
+  const createProcess = async (data: Partial<Process>) => {
+    try{
+      await registerProcess(data)
+      toast({
+        status: 'success',
+        title: 'Sucesso',
+        description: 'Processo criado com sucesso'
+      })
+
+      router.refresh()
+    }catch(error: any){
+      toast({
+        status: 'error',
+        title: 'Erro',
+        description: error.message
+      })
+    }
+  }
 
   return (
     <div className="m-5">
         <h1 className="text-5xl mb-16">Processos</h1>
+        <CreateButton>
+          <ProcessForm formTitle="Criar novo processo" onSubmit={createProcess}/>
+        </CreateButton>
         <ProcessesList/>
     </div>
   );
