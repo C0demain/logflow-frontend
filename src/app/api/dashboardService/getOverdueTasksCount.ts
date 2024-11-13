37197a68081e0bca90b7a3108536b0b1a72d10be
BrunoVieira003 <brunosilverio01@gmail.com>
2024-11-13 18:01:03 -0300

    feat: sync filtro de datas com tarefas atrasadas

--- src/app/api/dashboardService/getOverdueTasksCount.ts
+++ src/app/api/dashboardService/getOverdueTasksCount.ts
@@ -1,16 +1,33 @@
 import { createApiInstances } from "@/app/util/baseURL";
 import axios, { AxiosError } from "axios";
 
-export const getOverdueTasksCount = async (): Promise<number> => {
+interface Filters{
+  startedAt?: string | Date,
+  dueDate?: string | Date
+}
+
+export const getOverdueTasksCount = async (filters?: Filters): Promise<number> => {
   
   const { apiInstance } = await createApiInstances();
+  if(filters?.startedAt === ""){
+    filters.startedAt = undefined
+  }
+
+  if(filters?.dueDate === ""){
+    filters.dueDate = undefined
+  }
 
   try {
    
-    const response = await apiInstance.get("/api/v1/task/overdue/count");
+    const response = await apiInstance.get("/api/v1/task/overdue/count", {params:{
+      startedAt: filters?.startedAt,
+      dueDate: filters?.dueDate
+    }});
+    console.log(response)
+
     return response.data.count; 
   } catch (error) {
-    
+    console.log(error)
     if (axios.isAxiosError(error)) {
       throw new AxiosError(error.response?.data?.message || "Erro ao carregar tarefas atrasadas");
     } else {
