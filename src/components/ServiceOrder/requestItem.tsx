4b0fffe6b6a7c3e04b7d355eacd76332f2bc03b4
Cauã Dezidera <dezideracaua@gmail.com>
2024-11-29 10:06:11 -0300

    fix: tamanho e alinhamento dos botões de histórico e tarefas da ordem de serviços

--- src/components/ServiceOrder/requestItem.tsx
+++ src/components/ServiceOrder/requestItem.tsx
@@ -53,52 +53,53 @@ export const RequestItem: React.FC<RequestItemProps> = ({
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 p-4 sm:p-5 rounded-md w-full bg-white shadow-lg transition-all hover:bg-gray-100">
 
-      {/* Primeira Seção: Informações Básicas */}
-      <div className="flex flex-col justify-between col-span-1 sm:col-span-1 mb-4 sm:mb-0">
-        <div className="flex flex-col space-y-1 mb-4">
-          <h1 className="text-lg sm:text-2xl font-bold">{name}</h1>
-          <h1 className="text-base sm:text-lg">Cliente: {clientName}</h1>
-          <h1 className="text-base sm:text-lg">Preço: {price}</h1>
-        </div>
-        {crudAutorizado && <DeleteOrder id={orderId} />}
-      </div>
-
-      {/* Segunda Seção: Linha de Etapas */}
-      <div className="flex justify-center items-center col-span-1 sm:col-span-1">
-        <StageLine tasks={tasksList} />
-      </div>
-
-      {/* Terceira Seção: Status e Links */}
-      {userId && orderId && (
-        <div className="flex flex-col col-span-1 w-full justify-between items-end">
-          {/* Status Badge */}
-          <div className="flex justify-center mb-4">
-            <div
-              className={`badge h-6 ${status === "FINALIZADO" ? "badge-success" : "badge-info"
-                }`}
-            >
-              <p className="text-sm sm:text-base font-medium">{status}</p>
-            </div>
-          </div>
-
-          {/* Links */}
-          <div className="flex flex-col sm:flex-row items-center  sm:space-x-1 mt-4 sm:mt-0">
-            <Link
-              className="w-full sm:w-28 h-10 flex justify-center items-center rounded-md bg-blue-400 hover:bg-blue-500 transition-all mb-2 sm:mb-0"
-
-              href={`/auth/history`}
-            >
-              Histórico
-            </Link>
-            <Link
-              className="w-full sm:w-28 h-10 flex justify-center items-center rounded-md bg-blue-400 hover:bg-blue-500 transition-all"
-              href={`/auth/to-do-list/${orderId}`}
-            >
-              Tarefas
-            </Link>
-          </div>
-        </div>
-      )}
+  {/* Primeira Seção: Informações Básicas */}
+  <div className="flex flex-col justify-between col-span-1 sm:col-span-1 mb-4 sm:mb-0">
+    <div className="flex flex-col space-y-1 mb-4">
+      <h1 className="text-lg sm:text-2xl font-bold">{name}</h1>
+      <h1 className="text-base sm:text-lg">Cliente: {clientName}</h1>
+      <h1 className="text-base sm:text-lg">Preço: {price}</h1>
     </div>
+    {crudAutorizado && <DeleteOrder id={orderId} />}
+  </div>
+
+  {/* Segunda Seção: Linha de Etapas */}
+  <div className="flex justify-center items-center col-span-1 sm:col-span-1">
+    <StageLine tasks={tasksList} />
+  </div>
+
+  {/* Terceira Seção: Status e Links */}
+  {userId && orderId && (
+    <div className="flex flex-col col-span-1 w-full sm:items-end sm:text-right items-center text-center">
+      
+      {/* Status Badge */}
+      <div className="flex justify-center mb-4">
+        <div
+          className={`badge h-6 ${status === "FINALIZADO" ? "badge-success" : "badge-info"
+            }`}
+        >
+          <p className="text-sm sm:text-base font-medium">{status}</p>
+        </div>
+      </div>
+
+      {/* Links */}
+      <div className="flex flex-col sm:flex-row items-center sm:items-end sm:space-x-1 mt-4 sm:mt-0 lg:mt-20">
+        <Link
+          className=" px-6 py-3 sm:px-4 sm:py-2 w-full sm:w-28 h-10 flex justify-center items-center rounded-md bg-blue-400 hover:bg-blue-500 transition-all mb-2 sm:mb-0"
+          href={`/auth/history`}
+        >
+          Histórico
+        </Link>
+        <Link
+          className="w-full sm:w-28 h-10 flex justify-center items-center rounded-md bg-blue-400 hover:bg-blue-500 transition-all"
+          href={`/auth/to-do-list/${orderId}`}
+        >
+          Tarefas
+        </Link>
+      </div>
+    </div>
+  )}
+</div>
+
   );
 };
